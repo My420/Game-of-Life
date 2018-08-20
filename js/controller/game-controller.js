fc9cc8f317ee42fb4e0db1bb9a805b0eeb28ddf5
My420 <My420V@gmail.com>
2018-08-20 15:48:10 +0300

    refactoring controller

--- js/controller/game-controller.js
+++ js/controller/game-controller.js
@@ -3,13 +3,13 @@ import game from '../data/data.js';
 class GameController {
   constructor() {
     this.gameField = document.querySelector(`.field`);
-    this.nextStepButton = document.querySelector(`.controls__button--nextStep`);
-    this.playButton = document.querySelector(`.controls__button--play`);
-    this.clearButton = document.querySelector(`.controls__button--clear`);
-    this.stopButton = document.querySelector(`.controls__button--stop`);
-    this.randomButton = document.querySelector(`.controls__button--random`);
-    this.speedUpButton = document.querySelector(`.controls__button--speedUp`);
-    this.speedDownButton = document.querySelector(`.controls__button--speedDown`);
+    this.gameControls = document.querySelector(`.controls`);
+    this.wikiLink = document.querySelector(`.page_header__link`);
+    this._link = `https://ru.wikipedia.org/wiki/%D0%98%D0%B3%D1%80%D0%B0_%C2%AB%D0%96%D0%B8%D0%B7%D0%BD%D1%8C%C2%BB`;
+    this.nextStepButton = document.querySelector(`.controls__button[data-action=oneStep]`);
+    this.playButton = document.querySelector(`.controls__button[data-action=start]`);
+    this.speedUpButton = document.querySelector(`.controls__button[data-action=speedUp]`);
+    this.speedDownButton = document.querySelector(`.controls__button[data-action=speedDown]`);
   }
 
   _findCellNumber(evt) {
@@ -30,15 +30,40 @@ class GameController {
     this.speedDownButton.disabled = true;
   }
 
+  _onButtonClick(button) {
+   switch (button) {
+      case `start`:
+        game.play();
+        this._setFirstOptionList();
+        break;
+      case `oneStep`:
+        game.oneStep();
+        break;
+      case `pause`:
+        game.stop();
+        this._setSecondOptionList();
+        break;
+      case `reset`:
+        game.clear();
+        this._setSecondOptionList();
+        break;
+      case `random`:
+        game.random();
+        this._setSecondOptionList();
+        break;
+      case `speedUp`:
+        game.changeSpeed(`up`);
+        break;
+      case `speedDown`:
+        game.changeSpeed(`down`);
+        break;
+    };
+  }
+
   init() {
-    this.nextStepButton.addEventListener('click', ()=>{game.oneStep()});
-    this.playButton.addEventListener('click', ()=>{game.play(); this._setFirstOptionList()});
-    this.clearButton.addEventListener('click', ()=>{game.clear(); this._setSecondOptionList()});
-    this.stopButton.addEventListener('click', ()=>{game.stop(); this._setSecondOptionList()});
-    this.randomButton.addEventListener('click', ()=>{game.random(); this._setSecondOptionList()});
-    this.gameField.addEventListener('click', (evt)=>{game.changeCellStatus(...this._findCellNumber(evt))});
-    this.speedUpButton.addEventListener('click', ()=>{game.changeSpeed(`up`)});
-    this.speedDownButton.addEventListener('click', ()=>{game.changeSpeed(`down`)});
+    this.gameControls.addEventListener(`click`, (evt) => {if(evt.target.dataset.action){this._onButtonClick(evt.target.dataset.action)}});
+    this.gameField.addEventListener('click', (evt)=> {game.changeCellStatus(...this._findCellNumber(evt))});
+    this.wikiLink.addEventListener('click', (evt) => {evt.preventDefault(); window.open(this._link)});
     game.changeView();
   }
 };
